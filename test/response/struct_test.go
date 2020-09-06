a5fb8db665ac50918fe787c9966ba56ab92d7e4d
Reindert Vetter <reindert@myparcel.nl>
2020-09-06 17:35:31 +0200

    lanvard/lanvard#56 seperate reponse tests

--- test/response/struct_test.go
+++ test/response/struct_test.go
@@ -10,26 +10,6 @@ import (
 	"testing"
 )
 
-func TestJsonResponseFromEmptyString(t *testing.T) {
-	options := http.Options{App: foundation.NewApp()}
-	request := http.NewRequest(options)
-	response := middleware.ResponseJsonBody{}.Handle(request, func(request inter.Request) inter.Response {
-		return outcome.NewResponse(outcome.Options{})
-	})
-
-	assert.Equal(t, "", response.Content())
-}
-
-func TestJsonResponseFromJsonString(t *testing.T) {
-	options := http.Options{App: foundation.NewApp()}
-	request := http.NewRequest(options)
-	response := middleware.ResponseJsonBody{}.Handle(request, func(request inter.Request) inter.Response {
-		return outcome.Json("{\"height\": 12}")
-	})
-
-	assert.Equal(t, "{\"height\": 12}", response.Content())
-}
-
 func TestJsonResponseWithoutResponseEncoder(t *testing.T) {
 	options := http.Options{App: foundation.NewApp()}
 	request := http.NewRequest(options)
