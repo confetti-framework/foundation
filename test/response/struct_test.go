6460cf5b32d3e64f37fce5c3b27c1273890210d1
Reindert Vetter <reindert@myparcel.nl>
2020-09-06 18:12:08 +0200

    lanvard/lanvard#56 transform struct tot json

--- test/response/struct_test.go
+++ test/response/struct_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/lanvard/foundation/http"
 	"github.com/lanvard/foundation/http/middleware"
 	"github.com/lanvard/routing/outcome"
+	"github.com/lanvard/support/transformer"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -29,18 +30,41 @@ func TestJsonResponseWithoutResponseEncoder(t *testing.T) {
 	)
 }
 
-// func TestJsonResponseFromStruct(t *testing.T) {
-// 	options := http.Options{App: foundation.NewApp()}
-// 	request := http.NewRequest(options)
-// 	response := middleware.ResponseJsonBody{}.Handle(request, func(request inter.Request) inter.Response {
-// 		return outcome.Json(Foo{12})
-// 	})
-//
-// 	response.SetApp(request.App())
-//
-// 	assert.Equal(t, "{\"height\": 12}", response.Content())
-// }
+func TestNilCanTransformToJson(t *testing.T) {
+	assert.True(t, transformer.StructToJson{}.IsValid(nil))
+}
+
+func TestStringCanNotTransformToJson(t *testing.T) {
+	object := "foo"
+	assert.False(t, transformer.StructToJson{}.IsValid(object))
+}
+
+func TestIntCanNotTransformToJson(t *testing.T) {
+	object := 12
+	assert.False(t, transformer.StructToJson{}.IsValid(object))
+}
+
+func TestEmptyStructCanTransformToJson(t *testing.T) {
+	object := Foo{}
+	assert.True(t, transformer.StructToJson{}.IsValid(object))
+}
+
+func TestTransformUnsupportedTypeShouldPanic(t *testing.T) {
+	assert.PanicsWithValue(t, "can not transform to json with an unsupported type", func() {
+		transformer.StructToJson{}.Transform("foo")
+	})
+}
+
+func TestTransformStructWithIntToJson(t *testing.T) {
+	object := Foo{12}
+	assert.Equal(t, "{\"height\":12}", transformer.StructToJson{}.Transform(object))
+}
+
+func TestTransformStructWithFloat(t *testing.T) {
+	object := Foo{12.34}
+	assert.Equal(t, "{\"height\":12.34}", transformer.StructToJson{}.Transform(object))
+}
 
 type Foo struct {
-	Height float32
+	Height float32 `json:"height"`
 }
