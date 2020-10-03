0b481acdc25ebcc3338b40ec9a97ee34d338b349
Reindert Vetter <reindert@myparcel.nl>
2020-10-03 22:56:23 +0200

    lanvard/lanvard#67 get config.Path from container

--- providers/config_service_provider.go
+++ providers/config_service_provider.go
@@ -1,36 +1,15 @@
 package providers
 
 import (
-	"errors"
 	"github.com/lanvard/contract/inter"
-	"github.com/lanvard/support"
-	"strings"
 )
 
 type ConfigServiceProvider struct {
-	Index []interface{}
+	Index map[string]interface{}
 }
 
 func (c ConfigServiceProvider) Register(container inter.Container) inter.Container {
-	container.Instance("config", c.GetMap())
+	container.Instance("config", c.Index)
+
 	return container
-}
-
-func (c ConfigServiceProvider) GetMap() map[string]interface{} {
-	result := map[string]interface{}{}
-
-	for _, config := range c.Index {
-		result[getNameSpace(config)] = config
-	}
-
-	return result
-}
-
-func getNameSpace(config interface{}) string {
-	name := support.Name(config)
-	parts := strings.Split(name, ".")
-	if len(parts) < 2 {
-		panic(errors.New("config need to be a named struct. " + name + " given."))
-	}
-	return parts[1]
 }
