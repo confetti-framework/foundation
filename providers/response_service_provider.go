94c87c242b5e0c3e71e366c1eda069d971cab5d4
Reindert Vetter <reindert@myparcel.nl>
2020-10-25 23:37:43 +0100

    lanvard/lanvard#73 Clean up response decorators

--- providers/response_service_provider.go
+++ providers/response_service_provider.go
@@ -10,23 +10,22 @@ type ResponseServiceProvider struct{}
 
 func (c ResponseServiceProvider) Register(container inter.Container) inter.Container {
 	// Response decorators are responsible for modifying the response object.
-	// All these decorators will be used to customize the response object.
-	// Extent the decorators to further customize the response object
-	container.Bind("response_decorators", response_decorator.ResponseDecorators)
-	container.Extend("response_decorators", func(service interface{}) interface{} {
-		decorators := service.([]inter.ResponseDecorator)
-		decorators = append(
-			decorators,
-			// place your custom decorators here
-		)
-
-		return decorators
-	})
+	// All these decorators will be used.
+	container.Bind("response_decorators", append(
+		response_decorator.ResponseDecorators,
+		// add your custom decorators here
+	))
 
 	// Outcome encoders are responsible for converting an object
-	// to a string. One encoder will eventually be used.
-	container.Bind("outcome_html_encoders", outcome.HtmlEncoders)
-	container.Bind("outcome_json_encoders", outcome.JsonEncoders)
+	// to a string. One encoder will be used.
+	container.Bind("outcome_html_encoders", append(
+		outcome.HtmlEncoders,
+		// add your custom HTML encoders here
+	))
+	container.Bind("outcome_json_encoders", append(
+		outcome.JsonEncoders,
+		// add your custom JSON encoders here
+	))
 
 	return container
 }
