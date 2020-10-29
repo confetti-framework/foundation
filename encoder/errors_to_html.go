cc254b83bc813278380b19937c1f6473c13fbc94
Reindert Vetter <reindert@myparcel.nl>
2020-10-29 23:29:32 +0100

    lanvard/lanvard#73 get error from view

--- encoder/errors_to_html.go
+++ encoder/errors_to_html.go
@@ -5,6 +5,7 @@ import (
 	"github.com/lanvard/contract/inter"
 	"github.com/lanvard/errors"
 	"github.com/lanvard/support/str"
+	"golang.org/x/text/language"
 	"html/template"
 	"io/ioutil"
 )
@@ -18,7 +19,7 @@ func (e ErrorToHtml) IsAble(object interface{}) bool {
 	return ok
 }
 
-func (e ErrorToHtml) EncodeThrough(_ inter.App, object interface{}, _ []inter.Encoder) (string, error) {
+func (e ErrorToHtml) EncodeThrough(app inter.App, object interface{}, _ []inter.Encoder) (string, error) {
 	err, ok := object.(error)
 	if !ok {
 		return "", errors.New("can't convert object to html in error format")
@@ -26,29 +27,43 @@ func (e ErrorToHtml) EncodeThrough(_ inter.App, object interface{}, _ []inter.En
 
 	errorMessage := str.UpperFirst(err.Error())
 	if e.TemplateFile != "" {
-		buf := bytes.NewBufferString("")
 		status, _ := errors.FindStatus(err)
 		view := ErrorView{
-			Message: errorMessage,
-			Status:  status,
+			Message:      errorMessage,
+			Status:       status,
+			AppName:      app.Make("config.App.Name").(string),
+			Locale:       app.Make("config.App.Locale").(language.Tag).String(),
+			templatePath: e.TemplateFile,
 		}
 
-		message, err := ioutil.ReadFile(e.TemplateFile)
-		if err != nil {
-			return "", err
-		}
-		t, err := template.New("error").Parse(string(message))
-		if err != nil {
-			return "", err
-		}
-		err = t.Execute(buf, view)
-
-		return buf.String(), nil
+		return contentByView(view)
 	}
 	return errorMessage, nil
 }
 
+func contentByView(view interface{ Template() string }) (string, error) {
+	content, err := ioutil.ReadFile(view.Template())
+	buf := bytes.NewBufferString("")
+	if err != nil {
+		return "", err
+	}
+	t, err := template.New("error").Parse(string(content))
+	if err != nil {
+		return "", err
+	}
+	err = t.Execute(buf, view)
+
+	return buf.String(), nil
+}
+
 type ErrorView struct {
-	Message string
-	Status  int
+	Message      string
+	Status       int
+	AppName      string
+	Locale       string
+	templatePath string
+}
+
+func (v ErrorView) Template() string {
+	return v.templatePath
 }
