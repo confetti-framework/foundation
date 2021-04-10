0c4b4327091de43c26dd2b2eff0154b4a7217810
Vaggelis Yfantis <me@octoper.me>
2021-04-10 23:00:52 +0300

    fix command index ux

--- console/service/index.go
+++ console/service/index.go
@@ -16,12 +16,11 @@ func RenderIndex(c inter.Cli, commands []inter.Command) inter.ExitCode {
 	})
 
 	t := c.Table()
-	t.AppendRow([]interface{}{"\u001b[30;1mGlobal options:"})
+	t.AppendRow([]interface{}{"\u001B[32mGlobal options:\u001B[0m"})
 	t.AppendRow([]interface{}{"-h --help", "Show the command's available arguments."})
 	t.AppendRow([]interface{}{"--env-file", "Run the command with a defined environment file."})
-	t.AppendRow([]interface{}{"\u001B[0m "})
 
-	t.AppendRow([]interface{}{"\u001B[30;1mAvailable commands:\u001B[0m"})
+	t.AppendRow([]interface{}{"\n\u001B[32mAvailable commands:\u001B[0m"})
 
 	for _, command := range commands {
 		t.AppendRow([]interface{}{command.Name(), command.Description()})
